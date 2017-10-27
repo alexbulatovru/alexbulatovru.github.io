07136c9c37c8950eef9af2b32665bc22995a1494
alexbulatovru <bullexey@gmail.com>
2017-10-27 20:35:39 +0300

    commit

---
+++ main.js
@@ -0,0 +1,10 @@
+$('.sl').slick({
+	arrows: false,
+	dots: true
+});
+$('.sl_2').slick({
+	arrows: true,
+	prevArrow: '<div class="prev"></div>',
+	nextArrow: '<div class="next"></div>',	
+	dots: true
+});
